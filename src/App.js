5f57d47c66ca02d384570dbe66bfbb644ae91970
Hady Mohamed <hadymohamed739@gmail.com>
2026-01-11 18:15:04 +0200

    U06: Protect routes using localStorage login check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css';
 import First from './components/First';
 import Second from './components/second';
@@ -29,17 +29,66 @@ function App() {
         <Route path="/Forget" element={<Four />}></Route>
         <Route path="/otp" element={<Five/>}></Route>
         <Route path="/Reset" element={<Six />}></Route>
-        <Route path="/home" element={<Home/>}></Route>
+        <Route
+  path="/home"
+  element={
+    <ProtectedRoute>
+      <Home />
+    </ProtectedRoute>
+  }
+/>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/faq" element={<Faq/>}></Route>
-        <Route path="/Welcome" element={<Welcome/>}></Route>
+        <Route
+  path="/Welcome"
+  element={
+    <ProtectedRoute>
+      <Welcome />
+    </ProtectedRoute>
+  }
+/>
         <Route path="/aftermap" element={<AfterMap/>}></Route>
         <Route path="/book" element={<Book/>}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/donation" element={<Donation />}></Route>
-        <Route path="/emergency" element={<Emergency />}></Route>
-        <Route path="/request" element={<Request />}></Route>
-        <Route path="/map" element={<Map />}></Route>
+        <Route
+  path="/profile"
+  element={
+    <ProtectedRoute>
+      <Profile />
+    </ProtectedRoute>
+  }
+/>
+        <Route
+  path="/donation"
+  element={
+    <ProtectedRoute>
+      <Donation />
+    </ProtectedRoute>
+  }
+/>
+        <Route
+  path="/emergency"
+  element={
+    <ProtectedRoute>
+      <Emergency />
+    </ProtectedRoute>
+  }
+/>
+        <Route
+  path="/request"
+  element={
+    <ProtectedRoute>
+      <Request />
+    </ProtectedRoute>
+  }
+/>
+        <Route
+  path="/map"
+  element={
+    <ProtectedRoute>
+      <Map />
+    </ProtectedRoute>
+  }
+/>
       </Routes>
     </div>
   );
